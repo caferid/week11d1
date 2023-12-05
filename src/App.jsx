49e5c7afef40abe6086cf9846545a184fc378108
FidanHuseynova <tu544sn3v@code.edu.az>
2023-12-05 08:23:49 +0400

    deyisiklik

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import Todu from './testler/Todu'
+import { Calc } from './testler/redux_main'
+
+function App() {
+
+  return (
+    <>
+      <Calc></Calc>
+      {/* <Test_2></Test_2> */}
+      {/* <Todu></Todu> */}
+    </>
+  )
+}
+
+export default App
